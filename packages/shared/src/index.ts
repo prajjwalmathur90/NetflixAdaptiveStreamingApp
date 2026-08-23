b9919b6e8687468ad2ffc48115e020698ae60ef6
Prajjwal Mathur <pra90mathur@gmail.com>
2026-08-23 14:41:41 +0530

    Added basic db setup

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,8 @@
+export const TASK_QUEUE = "video-processing";
+export const PROCESS_VIDEO_WORKFLOW = "processVideoWorkflow";
 
-
-export {};
+export interface ProcessVideoInput {
+  videoId: string;
+  inputRelativePath: string;
+  outputRelativePath: string;
+}
